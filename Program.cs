a95ddbd2f77c8e787b676c8c9215b3ef3d65751f
sanjana k <sanjanarthi@gmail.com>
2021-03-11 12:39:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageDay6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Emp wage Computation Program");
+        }
+    }
+}
