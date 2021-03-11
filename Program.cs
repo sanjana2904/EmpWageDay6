10a856cba09418e40a35d003f8d7de017dc0dc14
sanjana k <sanjanarthi@gmail.com>
2021-03-11 12:52:08 +0530

    "Added UC3"

--- Program.cs
+++ Program.cs
@@ -6,20 +6,22 @@ namespace EmpWageDay6
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_FULL_TIME)
+            if (empCheck == IS_PART_TIME)
+                empHrs = 4;
+            else if (empCheck == IS_FULL_TIME)
                 empHrs = 8;
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: " + empWage);
+            Console.WriteLine("Emp wage:" + empWage);
             Console.ReadKey();
-
         }
     }
 }
